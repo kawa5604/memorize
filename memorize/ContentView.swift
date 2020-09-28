955bcc8de91966ade8cea4c6e8f644915a62f9c2
Jorge G <jorge.gar@me.com>
2020-09-27 23:34:58 -0700

    Initial Commit

---
+++ memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Jorge Garcia on 9/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Jorge Garcia on 9/27/20.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
